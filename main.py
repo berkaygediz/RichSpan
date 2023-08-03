4fece6bb42575d7a4b345d1362e980810ba1269c
Berkay Gediz <121058050+berkaygediz@users.noreply.github.com>
2023-08-03 04:56:18 +0300

    Added actions (right, left, center, justify, bold, italic, underline etc.).

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ class MainWindow(QMainWindow):
             'italic': False,
             'underline': False,
             'alignment': Qt.AlignLeft,
+            'text_color': '#000000',
+            'background_color': '#FFFFFF',
         }
 
         self.is_saved = False
@@ -24,26 +26,26 @@ class MainWindow(QMainWindow):
         self.text_editor = QTextEdit()
         self.text_editor.setFontFamily(self.default_values['font_family'])
         self.text_editor.setFontPointSize(self.default_values['font_size'])
-
+        self.text_editor.setTextColor(
+            QColor(self.default_values['text_color']))
+        self.text_editor.setTextBackgroundColor(
+            QColor(self.default_values['background_color']))
         self.status_bar = self.statusBar()
         self.status_bar.showMessage("Loading...", 2000)
 
-        self.dock_widget = QDockWidget("Docked Window", self)
+        self.dock_widget = QDockWidget("Details", self)
         self.statistics_label = QLabel()
         self.dock_widget.setWidget(self.statistics_label)
         self.dock_widget.setAllowedAreas(Qt.LeftDockWidgetArea)
-        self.dock_widget.setObjectName("Docked Window")
+        self.dock_widget.setObjectName("Details")
         self.addDockWidget(Qt.LeftDockWidgetArea, self.dock_widget)
         self.setCentralWidget(self.text_editor)
 
         self.update_statistics()
         self.text_editor.textChanged.connect(self.update_statistics)
 
-        action = QAction('&Action Text', self)
-        action.setStatusTip('Status Bar Text')
-        action.triggered.connect(self.action_triggered)
-        action.setShortcut('Ctrl+K')
-        self.addAction(action)
+        self.actionprepare()
+        self.toolbar()
 
     def update_window_title(self):
         file = self.file_name if self.file_name else "New File"
@@ -62,6 +64,62 @@ class MainWindow(QMainWindow):
 
     def action_triggered(self):
         self.status_bar.showMessage("Shortcut triggered.", 2000)
+
+    def alignrightevent(self):
+        self.text_editor.setAlignment(Qt.AlignRight)
+
+    def alignleftevent(self):
+        self.text_editor.setAlignment(Qt.AlignLeft)
+
+    def aligncenterevent(self):
+        self.text_editor.setAlignment(Qt.AlignCenter)
+
+    def alignjustifiedevent(self):
+        self.text_editor.setAlignment(Qt.AlignJustify)
+
+    def bold(self):
+        if self.text_editor.fontWeight() == QFont.Bold:
+            self.text_editor.setFontWeight(QFont.Normal)
+        else:
+            self.text_editor.setFontWeight(QFont.Bold)
+
+    def italic(self):
+        if self.text_editor.fontItalic():
+            self.text_editor.setFontItalic(False)
+        else:
+            self.text_editor.setFontItalic(True)
+
+    def underline(self):
+        if self.text_editor.fontUnderline():
+            self.text_editor.setFontUnderline(False)
+        else:
+            self.text_editor.setFontUnderline(True)
+
+    def actionprepare(self):
+        self.alignrightevent = QAction('Align Right', statusTip='Text rightward.',
+                                       triggered=self.alignrightevent, checkable=True, shortcut='Alt+R')
+        self.alignleftevent = QAction('Align Left', statusTip='Text leftward.',
+                                      triggered=self.alignleftevent, checkable=True, shortcut='Alt+L')
+        self.aligncenterevent = QAction('Align Center', statusTip='Text centered.',
+                                        triggered=self.aligncenterevent, checkable=True, shortcut='Alt+C')
+        self.alignjustifiedevent = QAction('Align Justify', statusTip='Text justified.',
+                                           triggered=self.alignjustifiedevent, checkable=True, shortcut='Alt+J')
+        self.bold = QAction('Bold', statusTip='Text bold.',
+                            triggered=self.bold, checkable=True, shortcut='Alt+B')
+        self.italic = QAction('Italic', statusTip='Text italic.',
+                              triggered=self.italic, checkable=True, shortcut='Alt+I')
+        self.underline = QAction('Underline', statusTip='Text underline.',
+                                 triggered=self.underline, checkable=True, shortcut='Alt+U')
+
+    def toolbar(self):
+        self.toolbar = self.addToolBar('Toolbar')
+        self.toolbar.addAction(self.alignrightevent)
+        self.toolbar.addAction(self.alignleftevent)
+        self.toolbar.addAction(self.aligncenterevent)
+        self.toolbar.addAction(self.alignjustifiedevent)
+        self.toolbar.addAction(self.bold)
+        self.toolbar.addAction(self.italic)
+        self.toolbar.addAction(self.underline)
 
 
 if __name__ == "__main__":
