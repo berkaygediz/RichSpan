c40a67b37b799bbbddd5cab53246d72bfe1364f0
Berkay Gediz <121058050+berkaygediz@users.noreply.github.com>
2024-02-08 05:34:12 +0300

    ⚰️imports

--- RichSpan.py
+++ RichSpan.py
@@ -7,7 +7,6 @@ import qtawesome as qta
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-from PyQt5.QtTest import *
 from PyQt5.QtPrintSupport import *
 from modules.translations import *
 
